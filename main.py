c89bb0105cf26edc4f74b58f9544be3e8b06182d
Said Khail <saidkhail@hotmail.com>
2026-04-14 10:52:17 +0100

    feat: add GET /logs with limit and offset pagination

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Query
 from database import engine, Base
 from datetime import datetime
 from sqlalchemy.orm import Session
 from contextlib import asynccontextmanager
 from models import LogRecord
 from schemas import LogCreate
+
 
 
 def create_database_and_tables():
@@ -22,6 +23,7 @@ def get_session():
     with Session(engine) as session:
         yield session
 
+# push the logs into the database
 @app.post("/logs")
 def create_log(log: LogCreate, session: Session = Depends(get_session)):
     db_log = LogRecord(
@@ -35,4 +37,18 @@ def create_log(log: LogCreate, session: Session = Depends(get_session)):
     session.refresh(db_log)
     return db_log 
 
-
+# get the logs information based on building simulated logs
+@app.get("/logs")
+def read_log(
+    session: Session = Depends(get_session),
+    offset: int = Query(default=0),
+    limit: int = Query(default=50, le=100)
+    ): 
+    logs = (
+        session.query(LogRecord)
+        .order_by(LogRecord.timestamp.desc())\
+        .offset(offset)\
+        .limit(limit).all()
+    )
+    return logs
+    
